319786faa293d7e1b5917668bba7b2caae8b7adc
amirbar <amirb4r@gmail.com>
2016-04-09 17:23:37 +0300

    archiving complex stuff for now. going with old implementation

--- old_code.js
+++ old_code.js
@@ -3,28 +3,28 @@
  */
 
 
- var http = require('http'),
- request = require('request'),
- cheerio = require('cheerio');
+var http = require('http'),
+    request = require('request'),
+    cheerio = require('cheerio');
 
- request('https://www.tripadvisor.com/Search?q=Bars&geo=293984&pid=3825&typeaheadRedirect=true&redirect=&startTime=undefined&uiOrigin=undefined&returnTo=__2F__', function (error, response, body) {
- if (!error && response.statusCode == 200) {
- var $ = cheerio.load(body);
- $('.result').each(function(_, place){
- var onClickText = $(place).attr("onclick");
- var link = onClickText.match(/.*((Restaurant|Attraction).*\.html).*/);
- if (link.length > 1) {
- request('https://www.tripadvisor.com/' + link[1], function (error, response, body) {
- if (!error && response.statusCode == 200) {
- var _$ = cheerio.load(body);
- var url = _$('.quote').each(function(_,entry){
- var url2 = _$(entry).find('a');
- if (typeof(url2) !== 'undefined' && typeof($(url2).attr('href') !== 'undefined'))
- console.log($(url2).attr('href'));
- });
- }
- });
- }
- });
- }
- });
+request('https://www.tripadvisor.com/Search?q=Bars&geo=293984&pid=3825&typeaheadRedirect=true&redirect=&startTime=undefined&uiOrigin=undefined&returnTo=__2F__', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(body);
+        $('.result').each(function (_, place) {
+            var onClickText = $(place).attr("onclick");
+            var link = onClickText.match(/.*((Restaurant|Attraction).*\.html).*/);
+            if (link.length > 1) {
+                request('https://www.tripadvisor.com/' + link[1], function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        var _$ = cheerio.load(body);
+                        var url = _$('.quote').each(function (_, entry) {
+                            var url2 = _$(entry).find('a');
+                            if (typeof(url2) !== 'undefined' && typeof($(url2).attr('href') !== 'undefined'))
+                                console.log($(url2).attr('href'));
+                        });
+                    }
+                });
+            }
+        });
+    }
+});
